6d4922a1bbd16a888b06ff1fa20233804d6c81d2
Joseph Craven <joecrav4@hotmail.com>
2022-09-26 09:31:42 +0100

    allow cross origin resource sharing

--- api/app.js
+++ api/app.js
@@ -5,10 +5,11 @@ const {
   pathErrorHandler,
 } = require("./controllers/error-handlers");
 const apiRouter = require("./routes/api-router.js");
-
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter)
